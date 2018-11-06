0003166dd2d3298c0a33baf1765184e6698f73ff
Iury F. A. Ferreira <iury.franklinferreira@gmail.com>
2018-11-06 09:06:39 -0300

    alteração de pagina inicial
    - Adição de scrollfade.js e time.js
    - Criação de slider na tela inicial
    - Adição de efeitos de scroll

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
 	"use strict"
 
 	// Scrollspy
@@ -8,18 +8,18 @@
 	});
 
 	// Mobile nav toggle
-	$('.navbar-toggle').on('click',function() {
+	$('.navbar-toggle').on('click', function () {
 		$('.main-nav').toggleClass('open');
 	});
 
 	// Fixed nav
-	$(window).on('scroll', function() {
+	$(window).on('scroll', function () {
 		var wScroll = $(this).scrollTop();
 		wScroll > 50 ? $('#header').addClass('fixed-navbar') : $('#header').removeClass('fixed-navbar');
 	});
 
 	// Smooth scroll
-	$(".main-nav a[href^='#']").on('click', function(e) {
+	$(".main-nav a[href^='#']").on('click', function (e) {
 		e.preventDefault();
 		var hash = this.hash;
 		$('html, body').animate({
@@ -28,17 +28,16 @@
 	});
 
 	// Section title animation
-	$('.section-title').each(function() {
+	$('.section-title').each(function () {
 		var $this = $(this);
-		$this.find('.title > span').each(function(i) {
+		$this.find('.title > span').each(function (i) {
 			var $span = $(this);
 			var animated = new Waypoint({
 				element: $this,
-				handler: function()
-				{
-					setTimeout(function(){
+				handler: function () {
+					setTimeout(function () {
 						$span.addClass('appear')
-					}, i*250);
+					}, i * 250);
 					this.destroy();
 				},
 				offset: '95%'
@@ -47,25 +46,48 @@
 	});
 
 	// Galery Owl
-	$('#galery-owl').owlCarousel({
-		items:1,
-		loop:true,
-		margin:0,
-		dots : false,
-		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		autoplay : true,
-		autoplaySpeed :500,
-		navSpeed :500,
-		responsive : {
-	    0 : {
-	       stagePadding : 0,
-	    },
-	    768 : {
-	        stagePadding : 120,
-	    }
-		}
+	var owl = $('#galery-owl')
+
+	owl.owlCarousel({
+		animateIn: 'fadeIn', // and this
+		animateOut: 'fadeOut',
+		items: 1,
+		loop: true,
+		margin: 0,
+		dots: false,
+		nav: false,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		autoplayTimeout: 1000,
+		responsive: {
+			0: {
+				stagePadding: 0,
+			},
+			768: {
+				stagePadding: 120,
+			}
+
+		},
+		onChanged: callBack
 	});
+
+
+
+	//Delay Segundo Slide
+	function callBack(event) {
+		// Solution to have correct item number. See: https://github.com/OwlCarousel2/OwlCarousel2/issues/1029. Thanks to Modicrumb
+		var current = (event.item.index + 1) - event.relatedTarget._clones.length / 2;
+		var allItems = event.item.count;
+		if (current > allItems || current <= 0) {
+			current = allItems - (current % allItems);
+		}
+
+		if (current == 1 || current == 3) {
+			owl.trigger('stop.owl.autoplay')
+			setTimeout(function () {
+				owl.trigger('play.owl.autoplay')
+			}, 5000)
+		}
+	}
 
 	// Parallax Background
 	$.stellar({
@@ -73,45 +95,23 @@
 	});
 
 	// CountTo
-	$('.counter').each(function() {
+	$('.counter').each(function () {
 		var $this = $(this);
 		var counter = new Waypoint({
 			element: $this,
-			handler: function()
-			{
+			handler: function () {
 				$this.countTo();
 			},
 			offset: '95%'
 		});
 	});
 
-    // Set the date we're counting down to
-    var countDownDate = new Date("Sep 5, 2019 15:37:25").getTime();
 
-    // Update the count down every 1 second
-    var x = setInterval(function() {
+	jQuery(window).load(function () {
+		$(".loader").fadeOut("slow");
+		$("#home").toggle("fast");
+	});
 
-        // Get todays date and time
-        var now = new Date().getTime();
-        
-        // Find the distance between now an the count down date
-        var distance = countDownDate - now;
-        
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-        // Output the result in an element with id="demo"
-        document.getElementById("timer").innerHTML ="<div class='start-in'>start in:</div>"+ days + "<span>days  </span>: " + hours + "<span>hour</span>: "
-        + minutes + "<span>mins  </span>: " + seconds + "<span>secs  </span>";
-        
-        // If the count down is over, write some text 
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("timer").innerHTML = "EXPIRED";
-        }
-    }, 1000);
+
 
 })(jQuery);
