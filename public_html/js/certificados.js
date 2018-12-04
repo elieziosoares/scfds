32988e6556a766380283b2a9f58d8bf7e493dfdd
iuryferreira <iury.franklinferreira@gmail.com>
2018-12-04 00:22:39 -0300

    Adição de certificados do minicurso

--- public_html/js/certificados.js
+++ public_html/js/certificados.js
@@ -1,11 +1,27 @@
 $('#botao_certificado').click(function () {
 
+
+    var dropdown = document.getElementById("select-edicao");
+    var valor_dropdown = dropdown.options[dropdown.selectedIndex].value;
     var cpf = document.getElementById("cpf").value;
-    var link = "certificados/{0}.pdf";
-    var link_final = link.replace("{0}", cpf);
+
+    if (valor_dropdown == "1") {
+
+        var link = "certificados/{0}.pdf";
+        var link_final = link.replace("{0}", cpf);
+    }
+
+    if (valor_dropdown == "2") {
+
+        var link = "certificados/minicurso/{0}.pdf";
+        var link_final = link.replace("{0}", cpf);
+    }
 
     //verificaUrl(link_final);
-    Download(link_final)
+    DownloadComChecagem(link_final)
+
+
+
 });
 
 function Download(link) {
@@ -13,7 +29,7 @@ function Download(link) {
     window.open(link, 'Download');
 }
 
-function DownloadComChecagem(link) {
+function DownloadComChecagem(link_final) {
 
   $.ajax({
       url:link_final,
@@ -23,7 +39,7 @@ function DownloadComChecagem(link) {
       },
       error: function() {
           //arquivo não existe
-          alert("sdsd");
+          alert("Certificado não encontrado. Verifique se inseriu os dados corretamente.");
       },
   });
 }
